e9ad5443e9c80dee4a11b7b6987160f2878ea898
eigenscribe <111261353+eigenscribe@users.noreply.github.com>
2024-05-16 01:04:00 -0400

    Create main.py

---
+++ Causal Inference/Chapter 2/Bookstore Example/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import stats
+from colors import bcolors as bc
+
+
+from bookstore_associations import *
+print()
+print()
+
+print()
+from bookstore_interventions_1 import *
